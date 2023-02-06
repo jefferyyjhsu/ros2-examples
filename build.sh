c8632ecaeebe435072202e88292f65eb99a30fca
Jeffery Hsu <yhsu@irobot.com>
2023-02-06 17:00:02 -0600

    add build.sh

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+colcon build --merge-install --packages-select examples_rclcpp_minimal_subscriber --cmake-force-configure --cmake-args -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DTHIRDPARTY=ON -DBUILD_TESTING:BOOL=OFF
