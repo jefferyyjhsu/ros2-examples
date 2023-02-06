e36db71e64f861f1873653d107aae49d764f44ba
Jeffery Hsu <yhsu@irobot.com>
2023-02-06 17:21:17 -0600

    clean up build/ and install/

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+rm -r build/ install/
 colcon build --merge-install --packages-select examples_rclcpp_minimal_subscriber --cmake-force-configure --cmake-args -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DTHIRDPARTY=ON -DBUILD_TESTING:BOOL=OFF
